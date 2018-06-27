d7ccad53de7fc072e0083a18b5d398a64006287f
Konstantina Tsarvouli <ntina_t@hotmail.com>
2018-06-27 12:12:13 +0300

    Write a new test suite named 'Initial Entries'

--- jasmine/spec/feedreader.js
+++ jasmine/spec/feedreader.js
@@ -71,9 +71,12 @@ $(function() {
           * should have two expectations: does the menu display when
           * clicked and does it hide when clicked again.
           */
+          
      });
 
     /* TODO: Write a new test suite named "Initial Entries" */
+    describe('Initial Entries', function() {
+
 
         /* TODO: Write a test that ensures when the loadFeed
          * function is called and completes its work, there is at least
@@ -81,6 +84,8 @@ $(function() {
          * Remember, loadFeed() is asynchronous so this test will require
          * the use of Jasmine's beforeEach and asynchronous done() function.
          */
+         
+    });     
 
     /* TODO: Write a new test suite named "New Feed Selection" */
 
